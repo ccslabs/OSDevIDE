e62dbc3d1ddad981598b0598c63d85129ff68991
ccslabs <ccslabs@hotmail.com>
2015-04-20 02:37:53 +0100

    Initial Commit

---
+++ OSDevIDE/frmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace OSDevIDE
+{
+    public partial class frmMain : Form
+    {
+
+        DockPanel dockPanel = new DockPanel();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            dockPanel.Dock = DockStyle.Fill;
+            dockPanel.BackColor = Color.AliceBlue;
+            toolStripContainer1.ContentPanel.Controls.Add(dockPanel);
+        }
+    }
+}
